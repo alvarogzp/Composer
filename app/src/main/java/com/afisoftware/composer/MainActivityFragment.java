e005b09ad64e470e171721f00750753f2db5ecd8
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2016-12-06 16:31:40 +0100

    Initial version

---
+++ app/src/main/java/com/afisoftware/composer/MainActivityFragment.java
@@ -0,0 +1,41 @@
+package com.afisoftware.composer;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    private MediaPlayer player;
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        ButterKnife.bind(this, view);
+        return view;
+    }
+
+    @OnClick(R.id.button)
+    public void playClicked() {
+        if (player != null) {
+            player.stop();
+        }
+        player = MediaPlayer.create(getActivity(), R.raw.music);
+        player.start();
+        Log.d("Composer", "Playing? " + player);
+    }
+}
