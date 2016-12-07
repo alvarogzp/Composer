115183a7aa92bbec63e98aafc0f212f0e0b2f245
Alvaro Gutierrez Perez <alvarogzp@users.noreply.github.com>
2016-12-07 11:48:42 +0100

    Add Button to choose music file, and improve playback UX

--- app/src/main/java/com/afisoftware/composer/MainActivityFragment.java
+++ app/src/main/java/com/afisoftware/composer/MainActivityFragment.java
@@ -1,8 +1,13 @@
 package com.afisoftware.composer;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -17,11 +22,18 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
+import static android.app.Activity.RESULT_OK;
+
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
     private static final int REFRESH_PLAYBACK_INTERVAL_MILLIS_WHILE_PLAYING = 10;
+
+    private static final int SELECT_FILE_CODE = 1;
+
+    @BindView(R.id.audio_name) TextView audioName;
+    @BindView(R.id.play_button) FloatingActionButton playButton;
 
     @BindView(R.id.playback_state) TextView playbackState;
     @BindView(R.id.playback_bar) SeekBar playbackBar;
@@ -31,6 +43,7 @@ public class MainActivityFragment extends Fragment {
     private Handler playbackRefreshHandler;
     private String totalPlaybackTime;
     private boolean draggingPlaybackBar = false;
+    private Uri audioUri;
 
     public MainActivityFragment() {
     }
@@ -54,8 +67,11 @@ public class MainActivityFragment extends Fragment {
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
                 draggingPlaybackBar = false;
-                if (isPlaying()) {
+                if (player != null) {
                     player.seekTo(seekBar.getProgress());
+                    if (!isPlaying()) {
+                        showPausedPlayback();
+                    }
                 } else {
                     showStoppedPlayback();
                 }
@@ -77,15 +93,64 @@ public class MainActivityFragment extends Fragment {
         playbackRefreshHandler = null;
     }
 
-    @OnClick(R.id.button)
-    public void playClicked() {
-        if (player != null) {
-            player.stop();
+    @OnClick(R.id.select_file_button)
+    public void selectFileButtonClicked() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("audio/*");
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+
+        try {
+            startActivityForResult(Intent.createChooser(intent, "Choose a music file"), SELECT_FILE_CODE);
+        } catch (ActivityNotFoundException e) {
+            Snackbar.make(getView(), "Please install a File Manager.", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null)
+                    .show();
         }
-        player = MediaPlayer.create(getActivity(), R.raw.music);
-        player.start();
-        Log.d("Composer", "Playing!");
-        startedPlayback();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case SELECT_FILE_CODE:
+                if (resultCode == RESULT_OK) {
+                    // Get the Uri of the selected file
+                    Uri uri = data.getData();
+                    audioName.setText(uri.toString());
+                    audioUri = uri;
+                    if (player != null) {
+                        player.stop();
+                        player = null;
+                    }
+                }
+                break;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @OnClick(R.id.play_button)
+    public void playClicked() {
+        if (isPlaying()) {
+            player.pause();
+        } else if (player != null) {
+            player.start();
+            resumedPlayback();
+        } else {
+            if (audioUri == null) {
+                player = MediaPlayer.create(getActivity(), R.raw.music);
+            } else {
+                player = MediaPlayer.create(getActivity(), audioUri);
+            }
+            player.start();
+            player.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mediaPlayer) {
+                    player = null;
+                    showStoppedPlayback();
+                }
+            });
+            Log.d("Composer", "Playing!");
+            startedPlayback();
+        }
     }
 
     private void startedPlayback() {
@@ -93,8 +158,12 @@ public class MainActivityFragment extends Fragment {
         playbackBar.setMax(totalPlaybackMillis);
         totalPlaybackTime = formatMillis(totalPlaybackMillis);
 
-        playbackState.setText("Playing");
+        resumedPlayback();
+    }
 
+    private void resumedPlayback() {
+        playbackState.setText("Playing");
+        playButton.setImageResource(android.R.drawable.ic_media_pause);
         refreshPlaybackScheduler();
     }
 
@@ -111,6 +180,8 @@ public class MainActivityFragment extends Fragment {
                         refreshPlaybackScheduler();
                     }
                 }, REFRESH_PLAYBACK_INTERVAL_MILLIS_WHILE_PLAYING);
+            } else if (player != null) {
+                showPausedPlayback();
             } else {
                 showStoppedPlayback();
             }
@@ -129,10 +200,17 @@ public class MainActivityFragment extends Fragment {
         playbackTime.setText(formatMillis(currentPlaybackMillis) + " / " + totalPlaybackTime);
     }
 
+    private void showPausedPlayback() {
+        playbackState.setText("Paused");
+        refreshPlayingPlayback();
+        playButton.setImageResource(android.R.drawable.ic_media_play);
+    }
+
     private void showStoppedPlayback() {
         playbackState.setText("Stopped");
         playbackBar.setProgress(0);
         playbackTime.setText(formatMillis(0) + " / " +  formatMillis(0));
+        playButton.setImageResource(android.R.drawable.ic_media_play);
     }
 
     private String formatMillis(int millis) {
